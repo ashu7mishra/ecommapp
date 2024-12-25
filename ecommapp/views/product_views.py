185034b1307059162ccbc6c724ac38588094d444
ashu7mishra <ashu7mishra@gmail.com>
2024-12-25 11:24:28 +0530

    added filters

--- ecommapp/views/product_views.py
+++ ecommapp/views/product_views.py
@@ -8,10 +8,10 @@ from ecommapp.models import Product
 from ecommapp.serializers.product_serializer import ProductSerializer
 
 
-class ProductAPIView(APIView):
+class ListCreateProductAPIView(APIView):
 
     def get(self, request):
-        products = Product.objects.all()
+        products = Product.objects.all().filter(price__in=[30, 100])
         serialized = ProductSerializer(products, many=True)
         return Response(serialized.data, status=200)
 
