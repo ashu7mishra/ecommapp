97c1acc7611fbc5fc880f0efadcce6f6a86a1125
ashu7mishra <ashu7mishra@gmail.com>
2024-12-25 09:20:02 +0530

    setup ecommapp

---
+++ ecommapp/views/product_views.py
@@ -0,0 +1,23 @@
+# from rest_framework.generics import GenericAPIView
+import base64
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ecommapp.models import Product
+from ecommapp.serializers.product_serializer import ProductSerializer
+
+
+class ProductAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        return Response({'products': products})
+
+    def post(self, request):
+        data = request.data
+        decoded_data = ProductSerializer(data=data)
+        if not decoded_data.is_valid():
+            return Response(decoded_data.errors, status=400)
+        decoded_data.save()
+        return Response(decoded_data.data, status=201)
