916119b1e7813345c5d216e2f7bee0230e984e7c
ashu7mishra <ashu7mishra@gmail.com>
2024-12-25 11:37:49 +0530

    custom queries

--- ecommapp/views/product_views.py
+++ ecommapp/views/product_views.py
@@ -11,7 +11,9 @@ from ecommapp.serializers.product_serializer import ProductSerializer
 class ListCreateProductAPIView(APIView):
 
     def get(self, request):
-        products = Product.objects.all().filter(price__in=[30, 100])
+        # products = Product.objects.all()
+        # products = Product.objects.all().filter(price__in=[30, 100])
+        products = Product.objects.raw("SELECT * FROM ecommapp_product")
         serialized = ProductSerializer(products, many=True)
         return Response(serialized.data, status=200)
 
