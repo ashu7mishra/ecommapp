63b420e34fa3a90459329be7367274fb4f5617f8
ashu7mishra <ashu7mishra@gmail.com>
2025-04-15 07:43:12 +0530

    created views

--- backend/services/users/serializers.py
+++ backend/services/users/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models import User
 from services.address.serializers import AddressSerializer
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     addresses = AddressSerializer(many=True, read_only=True)
+    password = serializers.CharField(write_only = True)
     
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'addresses']
+        fields = ['id', 'username', 'email', 'addresses', 'password']
+        read_only_fields = ['id']
+        
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- backend/services/users/views.py
+++ backend/services/users/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
-# Create your views here.
+User = get_user_model()
 
 class UserListCreateAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            return User.objects.all()
+        return User.objects.filter(id=user.id)
+    
+    def perform_create(self, serializer):
+        serializer.save()
+        
+        
+class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user
+    
+    def delete(self, request, *args, **kwargs):
+        
+        confirm = request.data.get('confirm', False)
+        
+        if not confirm:
+            raise ValidationError({"detail": "Please confirm account deletion by setting 'confirm': true."})
+        
+        self.request.user.delete()
+        return Response({"detail": "Account deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
     
