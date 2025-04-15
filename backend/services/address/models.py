7714ffebca9a095f9751602d7eba0cb5876fae9a
ashu7mishra <ashu7mishra@gmail.com>
2025-04-15 08:38:45 +0530

    created superuser and testing

--- backend/services/address/models.py
+++ backend/services/address/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
 
 class Address(models.Model):
     user = models.ForeignKey(User, related_name='addresses', on_delete=models.CASCADE)
