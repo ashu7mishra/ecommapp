56747fac1e84db53332bfd606afabe2ec4459c3e
ashu7mishra <ashu7mishra@gmail.com>
2025-04-19 18:27:38 +0530

    added product by category api

--- backend/services/product/urls.py
+++ backend/services/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
+from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView, ProductsByCategoryView
 
 
 urlpatterns = [
     path('', ProductListCreateAPIView.as_view(), name='product-list'),
-    path('', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+    path('<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+    path('category/<int:category_id>/', ProductsByCategoryView.as_view(), name='product-by-category'),
 ]
--- backend/services/product/views.py
+++ backend/services/product/views.py
@@ -3,6 +3,10 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import Product
 from .serializers import ProductSerializer
 from ..permissions import IsAdminOrReadOnly
+from rest_framework.views import APIView
+from ..category.models import Category
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
@@ -15,3 +19,14 @@ class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
     
+    
+class ProductsByCategoryView(APIView):
+    def get(self, request, category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+            products = Product.objects.filter(category=category)
+            serializer = ProductSerializer(products, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Category.DoesNotExist:
+            return Response({"detail": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
+        
