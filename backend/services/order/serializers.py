7367164424f9520718d0b3095a4446831c437873
ashu7mishra <ashu7mishra@gmail.com>
2025-04-22 07:58:46 +0530

    added serializers for order and orderItems

---
+++ backend/services/order/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from django.contrib.auth import get_user_model
+from ..product.models import Product
+
+
+User = get_user_model()
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.ReadOnlyField(source='Product.name')
+    
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_name', 'quantity', 'price']
+    
+    
+class OrderSerializer(serializers.ModelSerializer):
+    item = OrderItemSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'total_amount', 'status', 'created_at', 'updated_at', 'items']
+        read_only_fields = ['user', 'total_amount', 'status', 'created_at', 'updated_at']
+        
