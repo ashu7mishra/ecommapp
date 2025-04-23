d6f460c94fd32b2b8b059216b1950312a3d5fa72
ashu7mishra <ashu7mishra@gmail.com>
2025-04-23 08:13:00 +0530

    created serializers and views for Inventory and Notification services

--- backend/services/inventory/views.py
+++ backend/services/inventory/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Inventory
+from .serializers import InventorySerializer
 
-# Create your views here.
+
+class InventoryListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Inventory.objects.all()
+
+
+class InventoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Inventory.objects.all()
+    
--- backend/services/notification/views.py
+++ backend/services/notification/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationListCreateAPIViews(generics.ListCreateAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+class NotificationRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
