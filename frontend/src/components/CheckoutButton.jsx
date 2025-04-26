ee8e2eeca2e4cac099ad02b9feeb70d2b7c35158
ashu7mishra <ashu7mishra@gmail.com>
2025-04-26 15:21:21 +0530

    added checkout button

---
+++ frontend/src/components/CheckoutButton.jsx
@@ -0,0 +1,51 @@
+import api from '../api/axios.js'
+import { useEffect } from 'react'
+
+
+export default function CheckoutButton({ amount }) {
+
+    const handlePayment = async () => {
+        try {
+            // create razorpay order from backend
+            const res = api.post("payment/create-razorpay-order/", { amount });
+            const { order_id } = res.data
+
+            // open razorpay checkout
+            const options = {
+
+                key: import.env.VITE_RAZORPAY_KEY_ID,
+                amount: amount*100,
+                currency: "INR",
+                name: "EcommApp",
+                description: "Test Payment",
+                order_id: order_id,
+                handler: function (response) {
+                    console.log(response);
+                    alert("Payment successful");
+                },
+                prefill: {
+                    name: "Ashu Mishra",
+                    email: "test@example.com",
+                    contact: "7889451525",
+                },
+                theme: {
+                    color: "#3399cc",
+                },
+
+            };
+
+            const rzp = new window.Razorpay(options);
+            rzp.open()
+        } catch (error) {
+            console.log(error);
+            alert("something went wrong during payment creation");
+        }
+    };
+
+    return (
+        <button onClick={handlePayment}>
+            Pay ₹{amount}
+        </button>
+    )
+
+}
